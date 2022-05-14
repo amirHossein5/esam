3eec2c9bfcd37bd56a05600abbe9c5e428582ee7
amirHossein5 <adada5116@gmail.com>
2022-05-14 12:36:00 +0430

    update image and install request translator

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        "translate" => \AmirHossein5\RequestTranslator\Http\Middleware\RequestTranslatorMiddleware::class,
+        "translate_from" => \AmirHossein5\RequestTranslator\Http\Middleware\TranslateFromMiddleware::class,
         'toEnglishDigits' => \App\Http\Middleware\ToEnglishDigits::class,
         'json_decode' => \App\Http\Middleware\JsonDecode::class,
     ];
